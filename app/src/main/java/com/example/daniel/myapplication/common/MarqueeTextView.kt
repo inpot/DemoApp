fa34ad20350891df397014bdf0970d10cb9a5c47
Daniel.Luo <winepot2005@gmail.com>
2021-08-11 20:56:41 +0800

    update marquee textview

--- app/src/main/java/com/example/daniel/myapplication/common/MarqueeTextView.kt
+++ app/src/main/java/com/example/daniel/myapplication/common/MarqueeTextView.kt
@@ -1,62 +1,150 @@
-package com.example.daniel.myapplication.common
+package com.example.test
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.util.AttributeSet
 import android.util.Log
+import android.view.Gravity
 import android.view.animation.LinearInterpolator
 import android.widget.Scroller
 import androidx.appcompat.widget.AppCompatTextView
-import kotlin.math.roundToInt
 
-class MarqueeTextView(val ctx:Context,val attr: AttributeSet) :AppCompatTextView(ctx,attr){
+class MarqueeTextView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) :
+    AppCompatTextView(context, attrs, defStyle) {
+    private val TAG: String = "MarqueeTextView"
+    private var mRunning: Boolean = false
+    private val mScroller: Scroller = Scroller(context, LinearInterpolator())
+    private var mTxtWidth: Int = 0
+    private var mAttached: Boolean = false
+    private var mTxt: CharSequence? = null
+    private var mPreLoad: Boolean = false
+    private var mLoading: Boolean = false
+    private var mGravity: Int = 0
 
-    override fun onWindowFocusChanged(hasWindowFocus: Boolean) {
-        super.onWindowFocusChanged(hasWindowFocus)
-        if (hasWindowFocus) start() else stop()
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        Log.i(TAG, "onAttach width: $width")
+        mAttached = true
+        startScroll()
     }
 
-    var running = false
-    val du = 500L
-    var scr = Scroller(context,LinearInterpolator())
-    var txtWidth = 0
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        mAttached = false
+        stopScroll()
+    }
 
-    fun start() {
-        txtWidth = super.getPaint().measureText(text.toString()).toInt() + 100
-        Log.i("test","txtWidth ${txtWidth}")
-        if(txtWidth <= 0){
-            running = false
+    fun startScroll() {
+        if (mRunning) {
             return
         }
-        running = true
-        setScroller(scr)
+        mTxt = text
+        if (TextUtils.isEmpty(mTxt)) {
+            return
+        }
+        if (width <= 0) {
+            if (!mPreLoad) {
+                Log.i(TAG, "preload:$mPreLoad")
+                mPreLoad = true
+                post{ doStartScroll() }
+            }
+        } else {
+            if (!mLoading) {
+                mLoading = true
+                doStartScroll()
+                mLoading = false
+            }
+        }
+    }
+
+    private fun doStartScroll() {
+        mTxtWidth = (super.getPaint().measureText(mTxt.toString())).toInt()
+        val viewWidth: Int = width
+        Log.i(TAG, "txtWidth:$mTxtWidth viewWidth:$viewWidth")
+        if (mTxtWidth <= 0 || mTxtWidth <= viewWidth) {
+            return
+        }
+        mRunning = true
+        gravity = Gravity.START or  mGravity
+        setScroller(mScroller)
         startAnimation()
     }
 
-    val offset = 40
-
-    fun startAnimation(){
-        if(running){
-
-            postDelayed({
-                val startX:Int = if(scr.currX > txtWidth) -width else scr.currX
-                scr.startScroll(startX,0,offset,0,du.toInt())
-                invalidate()
-                startAnimation()
-            },du)
+    private fun startAnimation() {
+        if (!mRunning) {
+            return
         }
+        postDelayed(object :Runnable{
+
+            override fun run() {
+            val currX: Int = mScroller.currX
+            val startX: Int = if ((currX > mTxtWidth)) -width else currX
+            if (!mRunning) {
+                return
+            }
+            mScroller.startScroll(
+                startX, 0, OFFSET, 0,
+                DURATION.toInt()
+            )
+            invalidate()
+            startAnimation()
+            }
+        }, DURATION)
     }
 
-    fun stop(){
-        running = false
+    fun stopScroll() {
+        mRunning = false
+        mScroller.abortAnimation()
+        scrollTo(0, 0)
+        val curX: Int = mScroller.currX
+        mScroller.startScroll(curX, 0, -curX, 0, 1)
+        invalidate()
+        mScroller.abortAnimation()
+        mPreLoad = false
     }
 
     override fun computeScroll() {
         super.computeScroll()
-        if (scr.computeScrollOffset()) {
-            scrollTo(scr.getCurrX(),scr.getCurrY())
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(mScroller.currX, mScroller.currY)
             invalidate()
         }
     }
 
+    companion object {
+        private const val DURATION: Long = 500L
+        private const val OFFSET: Int = 40
+    }
 
+    init {
+        mGravity = gravity
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable) {
+                Log.i("test","$s")
+                if (mRunning) {
+                    stopScroll()
+                }
+                gravity = mGravity
+                val currX0: Int = scrollX
+                val currX: Int = mScroller.currX
+                val startX: Int = mScroller.startX
+                val finalX: Int = mScroller.finalX
+                Log.i(TAG, "currX0: $currX0 currX:$currX startX:$startX finalX: $finalX")
+                mTxt = s.toString()
+                if (mAttached) {
+                    startScroll()
+                }
+            }
+        })
+    }
 }
